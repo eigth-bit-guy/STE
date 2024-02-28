7216cbc1f06ddfec0eb8a3a93201be45bb73fbe6
samuel henrique dos santos <samuel@batCaverna.batCaverna.com.br>
2024-02-27 22:53:16 -0300

    feat: add correct reader

--- src/main.c
+++ src/main.c
@@ -1,12 +1,8 @@
-#include <GL/glut.h>
+#include <stdio.h>
+#include <GL/freeglut.h>
 
 int main(int argc, char *argv[])
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  glutInitWindowSize(350,300);
-  glutCreateWindow("Simple Text Editor");
-  glutMainLoop();
-
+  printf("Hello World!\n");
   return 0;
 }
