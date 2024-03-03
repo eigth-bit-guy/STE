d00fa6f1219716d01017372618bf672cd39038a1
samuel henrique dos santos <samuel@batCave.batCave.com.br>
2024-03-03 20:34:07 -0300

    fix: move freetype initialization

--- src/main.c
+++ src/main.c
@@ -9,22 +9,11 @@ void display(void)
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
 
+
 int main(int argc, char *argv[])
 {
   int g_Width = 600, g_Height = 600;
   glutInit(&argc, argv);
-  glutInitWindowSize (g_Width, g_Height);
-  glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowPosition(500, 500);
-  glutCreateWindow ("Simple Text Editor");
-  glutDisplayFunc(display);
-
-  GLenum res = glewInit();
-  if(res != GLEW_OK){
-    fprintf(stderr, "Error when initialize glew: %s\n", glewGetErrorString(res));
-    return 1;
-  }
-
   FT_Library ft;
   if(FT_Init_FreeType(&ft)){
     fprintf(stderr, "Error FreeType\n");
@@ -36,6 +25,17 @@ int main(int argc, char *argv[])
     return -1;
   }
   
+  glutInitWindowSize (g_Width, g_Height);
+  glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowPosition(500, 500);
+  glutCreateWindow ("Simple Text Editor");
+  glutDisplayFunc(display);
+
+  GLenum res = glewInit();
+  if(res != GLEW_OK){
+    fprintf(stderr, "Error when initialize glew: %s\n", glewGetErrorString(res));
+    return 1;
+  }
   glutMainLoop();
   return 0;
 }
