1e5587c0327883761b0b96f89c121b60efc89096
samuel henrique dos santos <samuel@batCave.batCave.com.br>
2024-03-03 18:41:16 -0300

    feat: create simple window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <GL/freeglut.h>
 
-int main()
+void display(void)
 {
-  printf("Hello World\n");
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+  int g_Width = 600, g_Height = 600;
+  glutInit(&argc, argv);
+  glutInitWindowSize (g_Width, g_Height);
+  glutInitWindowPosition(500, 500);
+  glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutCreateWindow ("Simple Text Editor");
+  glutDisplayFunc(display);
+  
+  glutMainLoop();
+
   return 0;
 }
+
