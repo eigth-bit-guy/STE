ef2b7408405794dbe0ac0881048600f4fb19c0f9
samuel henrique dos santos <samuel@batCave.batCave.com.br>
2024-03-29 00:13:40 -0300

    feat: remove old file and rename teste.c for main.c and add glfw

--- src/main.c
+++ src/main.c
@@ -1,50 +1,77 @@
+#include <stdio.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 #include <GL/glew.h>
-#include <GL/freeglut.h>
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-void display(void)
+void error_callback(int error, const char *description)
 {
-  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
+  fprintf(stderr, "Error: %s\n", description);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  int g_Width = 600, g_Height = 600;
-  glutInit(&argc, argv);
-  FT_Library ft;
-  if(FT_Init_FreeType(&ft)){
-    fprintf(stderr, "Error when intialize FreeType\n");
-    return -1;
-  }
-  FT_Face face;
-  if(FT_New_Face(ft, "..//fonts//FantasqueSansMono-Regular.ttf", 0, &face)){
-    fprintf(stderr, "Error failed to load font");
-    return -1;
-  }
-  FT_Set_Pixel_Sizes(face, 0, 48);
-  if(FT_Load_Char(face, 'X', FT_LOAD_RENDER)){
-    fprintf(stderr, "Error when load caracter\n");
-    return -1;
-  }
   
-  glutInitWindowSize (g_Width, g_Height);
-  glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowPosition(500, 500);
-  glutCreateWindow ("Simple Text Editor");
-  //Initialize glew
-  GLenum err = glewInit();
-  if(GLEW_OK != err){fprintf(stderr, "Error: %s\n", glewGetErrorString(err));}
-  glutDisplayFunc(display);
-
-  GLenum res = glewInit();
-  if(res != GLEW_OK){
-    fprintf(stderr, "Error when initialize glew: %s\n", glewGetErrorString(res));
-    return 1;
+  if(!glfwInit()){
+	printf("Erro when initialize glfw\n");
+	return 1;
   }
-  glutMainLoop();
+
+  GLFWwindow *window = NULL;
+  window = glfwCreateWindow(500, 500, "Simple Text Editor(STE)", NULL, NULL);
+  if(!window){
+	glfwTerminate();
+	return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  GLenum err = glewInit();
+  if(GLEW_OK != err){
+	fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+	return 1;
+  }
+
+  FT_Library library = {0};
+  FT_Face face = {0};
+  FT_Error error = FT_Init_FreeType(&library);
+  if(error){
+	//implement before
+	return 1;
+  }
+
+  error = FT_New_Face( library,
+                     "../fonts/FantasqueSansMono-Regular.ttf",
+                     0,
+                     &face );
+  //implement before
+  if ( error == FT_Err_Unknown_File_Format ){
+	return 1;
+  }
+  else if ( error ){
+	return 1;
+  }
+
+  error = FT_Set_Char_Size(
+						   face,    /* handle to face object         */
+						   0,       /* char_width in 1/64 of points  */
+						   16*64,   /* char_height in 1/64 of points */
+						   300,     /* horizontal device resolution  */
+						   300 );   /* vertical device resolution    */
+
+  error = FT_Set_Pixel_Sizes(
+							 face,   /* handle to face object */
+							 0,      /* pixel_width           */
+          16 );   /* pixel_height          */
+  
+  while(!glfwWindowShouldClose(window)){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glyph_index = FT_Get_Char_Index( face, charcode );
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+  }
+    
+  glfwTerminate();
   return 0;
 }
-
