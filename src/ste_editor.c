05b3540f82560c3787b66d32cdda79cdae74f1db
samuel henrique dos santos <samuel@batCave.batCave.com.br>
2024-03-29 01:29:10 -0300

    feat: add editor files for writing functions for editor

---
+++ src/ste_editor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "common.h"
+
+Errno read_input_file(void)
+{
+  //Implement before
+  return 0;
+}
---
+++ src/ste_editor.h
@@ -0,0 +1,10 @@
+#ifndef STE_EDITOR_H
+#define STE_EDITOR_H
+
+#include <stdlib.h>
+#include "common.h"
+
+//Implement before
+Errno read_input_file(void);
+
+#endif
